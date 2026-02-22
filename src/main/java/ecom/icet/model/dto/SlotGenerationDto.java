68697df64c82c2a2bcf474b229c50c95a867d95a
Kaveesha420 <icetkaveesha@gmail.com>
2026-02-22 14:33:05 +0530

    created SlotGenerationDto Dto class

---
+++ src/main/java/ecom/icet/model/dto/SlotGenerationDto.java
@@ -0,0 +1,20 @@
+package ecom.icet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SlotGenerationDto {
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int durationMinutes;
+}
